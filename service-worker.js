4d73692228618f73ffb7ef81893b3eb9e30b0e0a
voss1980-hue <voss.1980@gmail.com>
2025-09-22 20:50:19 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = 'sudoku-v1';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
